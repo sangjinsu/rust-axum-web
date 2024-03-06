9fb298c1b9d3c188ba16319e9173b4e35a2133da
sangjinsu <climbplant39@kakao.com>
2024-03-06 20:42:06 +0900

    ✨ Hello World!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused)]
+
+
+use std::net::SocketAddr;
+use axum::response::Html;
+use axum::Router;
+use axum::routing::get;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route(
+        "/hello",
+        get(|| async {
+            Html("Hello World")
+        }),
+    );
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
